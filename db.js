3cb07e2affdd99410a2828d1a6a8e2db2f472cef
RuslanPol <vvrv@yandex.ru>
2024-08-07 16:37:08 +0700

    test

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const { Pool } = require('pg');
-
+console.log(process.env.COLORTERM)
+console.log(process.env.DATABASE_URL)
 const pool = new Pool({
 	connectionString: process.env.DATABASE_URL,
 	ssl: {
